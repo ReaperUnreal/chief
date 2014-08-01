9f5d6a8c66f33d108bbd291e33a4782edf596d77
Casey Banner <kcbanner@gmail.com>
2014-08-01 00:27:20 -0400

    YouTube plugin works!

--- src/main/java/ca/caseybanner/chief/commands/YouTubeCommand.java
+++ src/main/java/ca/caseybanner/chief/commands/YouTubeCommand.java
@@ -8,10 +8,20 @@ package ca.caseybanner.chief.commands;
 
 import ca.caseybanner.chief.Bot;
 import ca.caseybanner.chief.Command;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
@@ -19,13 +29,22 @@ import java.util.regex.Pattern;
  */
 public class YouTubeCommand extends Command {
 
-	private static final Pattern PATTERN = Pattern.compile("^youtube\\s+(.+)$");
+	private static final Logger logger = LogManager.getLogger(YouTubeCommand.class);
 
-	private YouTube youtube;		
+	private static final Pattern PATTERN = Pattern.compile("^youtube\\s+(.+)$");
+	private YouTube youtube;	
 	private String apiKey;
 	
 	public YouTubeCommand(Bot bot) {
 		super(bot);
+		
+		youtube = new YouTube.Builder(
+				new NetHttpTransport(),
+				new JacksonFactory(),
+				(HttpRequest request) -> {})
+			.setApplicationName("chief-bot")
+			.build();
+		
 	}
 
 	/**
@@ -49,9 +68,32 @@ public class YouTubeCommand extends Command {
 	@Override
 	public Optional<String> processMessage(String from, String message, Matcher matcher) {
 		
-		String query = matcher.group(1);
+		String query = matcher.group(1);				
 		
-		
+		try {
+			YouTube.Search.List search = youtube.search().list("id,snippet");
+			search.setKey(this.apiKey);
+			search.setQ(query);
+			search.setType("video");
+			
+			search.setFields("items(id/kind,id/videoId,snippet/title,snippet)");
+			search.setMaxResults(1L);		
+			SearchListResponse searchResponse = search.execute();
+			
+			List<SearchResult> searchResultList = searchResponse.getItems();
+			if (searchResultList != null) {
+				SearchResult result = searchResultList.get(0);
+				ResourceId id = result.getId();
+				
+				if (id.getKind().equals("youtube#video")) {
+					return Optional.of("http://www.youtube.com/watch?v=" + id.getVideoId());
+				}				
+            } else {
+				return Optional.of("Sorry, no results for `" + query + "`!");
+            }						
+		} catch (IOException ex) {
+			logger.error("Error searching YouTube", ex);
+		}
 		
 		return Optional.empty();
 		
